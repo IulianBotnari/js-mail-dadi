6f953baa482c1304081d825ae1d57ed3238494c9
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-03 14:48:27 +0200

    initial commit + email

---
+++ main.js
@@ -0,0 +1,16 @@
+let userList = ["tizio, caio, sempronio"]
+
+let user = prompt("Inserisci user")
+
+let emailUser
+
+
+if (user = userList[0] || [1] || [2]){
+    emailUser = prompt("Inserisci mail")
+    console.log("email inserita");
+    
+} else {
+    console.log("ci hai provato");
+    
+}
+
