688f589cae815bdb09f5e0ff33e1bfeb3f8faf76
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-03 15:07:25 +0200

    Add Gioco dei dadi

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-let userList = ["tizio, caio, sempronio"]
 
-let user = prompt("Inserisci user")
+//email
 
-let emailUser
+// let userList = ["tizio, caio, sempronio"]
+
+// let user = prompt("Inserisci user")
+
+// let emailUser
 
 
-if (user = userList[0] || [1] || [2]){
-    emailUser = prompt("Inserisci mail")
-    console.log("email inserita");
+// if (user = userList[0] || [1] || [2]){
+//     emailUser = prompt("Inserisci mail")
+//     console.log("email inserita");
     
-} else {
-    console.log("ci hai provato");
+// } else {
+//     console.log("ci hai provato");
+    
+// }
+
+// gico dei dadi
+
+let computer = Math.floor(Math.random() * 6) + 1
+
+let user = Math.floor(Math.random() * 6) + 1
+
+
+if (computer > user){
+
+    console.log(`Computer vince! ${computer}`);
+    
+} else if (user > computer){
+    console.log(`User vince! ${user}`);
+
+} else{
+    console.log("parita");
     
 }
-
